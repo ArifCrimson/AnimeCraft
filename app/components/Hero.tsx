a76abf50192af551b0272cdbc80e3aef4c0cc3e8
ArifCrimson <hakimiarif132@gmail.com>
2025-05-02 11:48:19 +0800

    Created Hero Section (Not done yet)

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,3 +1,18 @@
 export default function Hero(){
-    
+    return (
+        <section className="w-full px-6 py-16 md:py-24 bg-zinc-900 text-white rounded-2xl mt-6 shadow-md">
+            <div className="max-w-6xl mx-auto rid grid-cols-1 md:grid-cols-2 items-center gap-10">
+                <div className="space-y-6">
+                    <h1 className="text-4xl md:text-5xl font-bold leading-tight">
+                        Unleashed Precision. <br /> Live Your Style.
+                    </h1>
+                    <p className="text-zinc-400 text-lg max-w-md">Premium anime-inspired jersey crafted for performance and presence.</p>
+                    <div className="flex gap-4">
+                        <button className="bg-red-500 hover:bg-red-400 transition text-white px-6 py-3 rounded-xl font-semibold">Contact Now</button>
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+    )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Navbar from "./components/navbar";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-stone-950 flex justify-center py-10 px-4">
       <div className="w-full max-w-7xl bg-zinc-800 rounded-2xl shadow-lg p-6 md:p-10">
         <Navbar />
+        <Hero/>
       </div>
     </main>
   );
