cca8d852370bc0386d616b9f265994b508fe4ce8
ArifCrimson <hakimiarif132@gmail.com>
2025-05-04 10:20:04 +0800

    little adjustment again for the hero, looking acceptable but not good enough

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <section className="relative w-full px-6 py-16 md:py-24 bg-zinc-900 rounded-4xl mt-6 shadow-md overflow-hidden">
-      <img src="/shirt.webp" alt="Anime Background" className="absolute inset-0 w-full h-full object-cover z-0" />
+      <img src="/shirt.webp" alt="Anime Background" className="absolute inset-0 w-full h-full object-contain z-0" />
       {/* <Image
         src="/shirt.webp"
         alt="Gojo shirt"
@@ -12,15 +12,15 @@ export default function Hero() {
         className="absolute inset-0 w-full h-full object-cover z-0"
       /> */}
 
-      <div className="relative z-20 max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 items-center gap-10">
-        <div className="space-y-6">
-          <h1 className="text-4xl md:text-5xl font-bold leading-tight">
+      <div className="relative z-20 max-w-6xl mx-auto h-full flex items-end justify-start">
+        <div className="bg-black/60 p-6 md:p-10 rounded-xl m-4 md:m-10">
+          <h1 className="text-3xl md:text-4xl font-bold leading-tight">
             Unleashed Precision. <br /> Live Your Style.
           </h1>
-          <p className="text-zinc-400 text-lg max-w-md">
+          <p className="text-zinc-400 text-lg max-w-md mt-4">
             Premium anime-inspired jersey crafted for performance and presence.
           </p>
-          <div className="flex gap-4">
+          <div className="mt-6">
             <button className="bg-red-500 hover:bg-red-400 transition text-white px-6 py-3 rounded-xl font-semibold">
               Contact Now
             </button>
