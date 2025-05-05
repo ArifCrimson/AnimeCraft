6468291c88856dc888ef854ef3b225df4813af53
ArifCrimson <hakimiarif132@gmail.com>
2025-05-05 11:24:43 +0800

    comment dark/light mode button in navbar. fix ul component so it not move to right

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -12,18 +12,18 @@ export default function Navbar(){
     const [darkMode, setDarkMode] = useState(true);
 
     return(
-        <nav className="bg-gray-950 px-6 py-4 flex justify-between items-center text-white shadow-sm">
+        <nav className="relative bg-gray-950 px-6 py-4 flex justify-between items-center text-white shadow-sm">
             <div className={`text-2xl font-bold tracking-wide ${krona.className}`}>
                 AnimeCraft
             </div>
 
-            <ul className={`flex gap-6 text-sm md:text-base ${krona.className}`}>
+            <ul className={`absolute left-1/2 transform -translate-x-1/2 flex gap-6 text-sm md:text-base ${krona.className}`}>
                 <li className="hover:text-red-500 transition cursor-pointer">Home</li>
                 <li className="hover:text-red-500 transition cursor-pointer">Products</li>
                 <li className="hover:text-red-500 transition cursor-pointer">Contact</li>
             </ul>
-
-            <button onClick={() => setDarkMode(!darkMode)} className="p-2 rounded-full hover:bg-zinc-700 transition" aria-label="Toggle dark mode"> {darkMode ? <Moon size={20} /> : <Sun size={20}/>} </button>
+            
+            {/* <button onClick={() => setDarkMode(!darkMode)} className="p-2 rounded-full hover:bg-zinc-700 transition" aria-label="Toggle dark mode"> {darkMode ? <Moon size={20} /> : <Sun size={20}/>} </button> */}
         </nav>
     )
 }
