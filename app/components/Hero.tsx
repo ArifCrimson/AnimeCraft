e473cf4b28b366f4ecfad209ed8da343a11b1b6b
ArifCrimson <hakimiarif132@gmail.com>
2025-05-05 10:35:58 +0800

    Updated the Hero section to look better

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative w-full px-6 py-16 md:py-24 bg-zinc-900 rounded-4xl mt-6 shadow-md overflow-hidden">
-      <img src="/shirt.webp" alt="Anime Background" className="absolute inset-0 w-full h-full object-contain z-0" />
+    <section className="relative w-full h-[500px] bg-zinc-900 rounded-2xl mt-6 shadow-md overflow-hidden">
+      <img src="/shirt.webp" alt="Anime Background" className="absolute inset-0 w-full h-full object-cover z-0" />
       {/* <Image
         src="/shirt.webp"
         alt="Gojo shirt"
@@ -12,12 +12,12 @@ export default function Hero() {
         className="absolute inset-0 w-full h-full object-cover z-0"
       /> */}
 
-      <div className="relative z-20 max-w-6xl mx-auto h-full flex items-end justify-start">
+      <div className="relative z-20 max-w-6xl mx-auto px-6 py-16">
         <div className="bg-black/60 p-6 md:p-10 rounded-xl m-4 md:m-10">
-          <h1 className="text-3xl md:text-4xl font-bold leading-tight">
+          <h1 className="text-4xl md:text-5xl font-bold leading-tight">
             Unleashed Precision. <br /> Live Your Style.
           </h1>
-          <p className="text-zinc-400 text-lg max-w-md mt-4">
+          <p className="text-zinc-300 text-lg max-w-xl mt-4">
             Premium anime-inspired jersey crafted for performance and presence.
           </p>
           <div className="mt-6">
