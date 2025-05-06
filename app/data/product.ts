177fad7ad3d7b9e45ca002233a7afa19b9ca60fb
ArifCrimson <hakimiarif132@gmail.com>
2025-05-06 10:08:05 +0800

    Added more products data and images.

--- app/data/product.ts
+++ app/data/product.ts
@@ -11,5 +11,17 @@ export const products: Product[] = [
         name: "Gojo Satoru Shirt",
         image: "/shirt.webp",
         description: "Premium quality shirt inspired of Jujutsu Kaisen characters named Gojo Satoru.",
+    },
+    {
+        id: 2,
+        name: "Keychains",
+        image: "/rjz28.webp",
+        description: "Cute and Charming Anime Keychains",
+    },
+    {
+        id: 3,
+        name: "Naruto Cosplay Accessories Set",
+        image: "/accessoriesSet.webp",
+        description: "Sora Tuki 55Pcs Sasori Headband Ring Necklace Bracelet Stickers Key Chain Set, Anime Fans Cosplay Accessory",
     }
 ];
